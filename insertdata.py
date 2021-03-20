b3b41ca0c7460c7a62d1afb44400b86ac8fc6e33
AnishDeshpande1 <anishdeshpande1999@gmail.com>
2021-03-20 21:20:49 +0530

    Add files via upload

--- insertdata.py
+++ insertdata.py
@@ -14,6 +14,7 @@ start_date = datetime.date(2020,1,1)
 end_date = datetime.date(2020,12,31)
 delta = datetime.timedelta(days = 1)
 count = 0
+sdate = start_date
 while start_date<=end_date:
 	date.append(start_date)
 	start_date+=delta
@@ -34,9 +35,11 @@ with open("accounts.sql",'w') as file:
 
 
 #FOR Ingredients
-with open("ingredients.sql",'w') as file:
+num_ingredients = 0
+with open("ingedients.sql",'w') as file:
 	with open("ingredients.txt",'r') as ifile:
 		for line in ifile:
+			num_ingredients = num_ingredients + 1
 			l2 = line[:-1]
 			l = l2.split(',')
 			sql = sql1 + "ingredients" + sql2 + l[0] + ',' + '\'' +l[1] + '\'' + sql3
@@ -45,10 +48,12 @@ with open("ingredients.sql",'w') as file:
 
 #For dishes
 i = 1
+num_dishes = 0
 with open("dishes.sql", 'w') as file:
 	with open("dishes.txt",'r') as ifile:
 		for line in ifile:
 			#print(line)
+			num_dishes = num_dishes +1
 			l2 = line[:-1]
 			l = l2.split(',')
 			#print(l)
@@ -63,27 +68,38 @@ with open("dishes.sql", 'w') as file:
 m_name = np.loadtxt("male_name.txt", dtype = str)
 f_name = np.loadtxt("female_name.txt", dtype = str)
 s_name = np.loadtxt("surname.txt", dtype = str)
+num_customers = 1000
+with open("customer_contact.sql",'w') as f2:
+	with open("customers.sql",'w') as file:
+		for i in range(num_customers):
+			g = np.random.uniform()
+			if(g<0.5):
+				fn = np.random.choice(m_name)
+				sex = 'm'
+			else:
+				fn = np.random.choice(f_name)
+				sex = 'f'
+			sn = np.random.choice(s_name)
+			age = int(np.random.uniform()*60) + 18  
+			g = np.random.uniform()
+			if(g<0.5):
+				mn = np.random.choice(f_name)
+			else:
+				mn = np.random.choice(m_name)
+			of = int(np.random.uniform()*10)
+			s = "'%s','%s','%s','%s','%s','%s','%s'" % (str(i+1),fn,mn,sn, sex, str(age),str(of))
+			sql = sql1+ "customers" + sql2 + s + sql3
+			file.write(sql)
+			first = str(random.randint(700, 999))
+			second = str(random.randint(1, 888)).zfill(3)
+			last = (str(random.randint(1, 9998)).zfill(4))
+			while last in ['1111', '2222', '3333', '4444', '5555', '6666', '7777', '8888']:
+				last = (str(random.randint(1, 9998)).zfill(4))
+			contact = '{}{}{}'.format(first, second, last)
+			contact = '\'' + contact + '\''
+			sql = sql1 + "customers_contact" + sql2 + contact + ',' + str(i+1) + sql3
+			f2.write(sql)
 
-with open("customers.sql",'w') as file:
-	for i in range(1000):
-		g = np.random.uniform()
-		if(g<0.5):
-			fn = np.random.choice(m_name)
-			sex = 'm'
-		else:
-			fn = np.random.choice(f_name)
-			sex = 'f'
-		sn = np.random.choice(s_name)
-		age = int(np.random.uniform()*60) + 18  
-		g = np.random.uniform()
-		if(g<0.5):
-			mn = np.random.choice(f_name)
-		else:
-			mn = np.random.choice(m_name)
-		of = int(np.random.uniform()*10)
-		s = "'%s','%s','%s','%s','%s','%s','%s'" % (str(i+1),fn,mn,sn, sex, str(age),str(of))
-		sql = sql1+ "customers" + sql2 + s + sql3
-		file.write(sql)
 
 #FOR THE STAFF
 
@@ -190,8 +206,118 @@ with open("staff_contact.sql",'w') as file:
 			last = (str(random.randint(1, 9998)).zfill(4))
 		contact = '{}{}{}'.format(first, second, last)
 		contact = '\'' + contact + '\''
-		sql = sql1 + "staff_contact" + sql2 + contact + ',' + str(i+1)
+		sql = sql1 + "staff_contact" + sql2 + contact + ',' + str(i+1) + sql3
 		file.write(sql)
+
+
+num_tables = 10
+size = [2,2,2,4,4,4,6,6,8,8]
+status = "finished"
+with open("sit.sql",'w') as file:
+	for i in range(num_tables):
+		loc = np.random.choice(["windows_side","centre","edge"])
+		s = str(i+1)+','+str(size[i])+',\''+status+'\',\''+loc+'\''
+		sql = sql1+"sit_table"+sql2+s+sql3
+		file.write(sql)
+	for i in range(50):
+		t_id = str(int(np.random.uniform()*10)+1)
+		sql = sql1 + "sitting" + sql2 + str(i+1) + ',' + t_id + sql3
+		file.write(sql)
+
+num_waiting = 30
+num_not_waiting = 200
+pay = [ "credit card","upi","debit card" ,"cash" ,"other"]
+with open("customers.sql",'a') as file:
+	ids = random.sample(list(np.arange(num_customers)+1),num_waiting+num_not_waiting)
+	for i in range(num_waiting+num_not_waiting):
+		if(i<num_waiting):
+			sql = sql1 + "waiting_customer" + sql2 + str(ids[i]) + ',' + str(i+1) + sql3
+			file.write(sql)
+		else:
+			p = np.random.choice(pay)
+			sql = sql1 + "non_waiting_customer" + sql2 + str(ids[i]) + ',' + str(i+1) + ',\'' + p + '\'' +sql3
+			file.write(sql)
+
+with open("contains.sql", 'w') as file:
+	for i in range(num_dishes):
+		item_per_dish = int(np.random.uniform(5,10,1))
+		ids = random.sample(list(np.arange(num_ingredients)+1),item_per_dish)
+		for j in range(len(ids)):
+			q = np.random.choice([1,2,3,4,5])
+			sql = sql1+"contains"+sql2+str(ids[j])+','+str(i+1)+','+str(q)+sql3
+			file.write(sql)
+
+num_orders = 100
+dic = {0:'monday',1:'tuesday',2:'wednesday',3:'thursday',4:'friday',5:'saturday',6:'sunday'}
+custs = random.sample(list(np.arange(num_customers)+1),num_orders)
+
+with open("orders.sql",'w') as file:
+	for i in range(num_orders):
+		d_id = np.random.choice(list(np.arange(num_dishes)+1))
+		rev = np.random.choice([1,2,3,4,5])
+		quan = np.random.choice([1,2,3,4])
+		cost = np.random.choice(list(np.arange(40,500,10)))
+		delt = int(np.random.uniform()*365)
+		delta = datetime.timedelta(days = delt)
+		sd = sdate+delta
+		day = dic[int(np.random.uniform()*6+1)]
+		h = int(np.random.uniform()*13)+9
+		m = int(np.random.uniform()*60)
+		time = datetime.time(h,m)
+		s = "'%s','%s','%s','%s','%s','%s','%s','%s','%s'" % (str(i+1),str(custs[i]),str(d_id),str(sd),str(time),day,str(quan),str(rev),str(cost))
+		sql = sql1+"orders"+sql2+s+sql3
+		file.write(sql)
+
+with open("cooks.sql",'w') as file:
+	for i in range(num_orders):
+		d_id = np.random.choice(list(np.arange(num_dishes)+1))
+		j = np.random.choice(list(np.arange(num_chefs)+num_owners+num_managers+num_receptionists+num_waiters))
+		completed = "1"
+		s = "'%s','%s','%s','%s'" % (str(j),str(i+1),str(d_id),completed)
+		sql = sql1+"cooks"+sql2+s+sql3
+		file.write(sql)
+
+with open("pay_by.sql",'w') as file:
+	for i in range(num_orders):
+		p = np.random.choice(pay)
+		i_id = 10000+i
+		i_des = "lorem ipsum"
+		status = np.random.choice(["paid","paid","paid","paid","paid","paid","paid","in progress","in progress","in progress","in progress","failed"])
+		s = "'%s','%s','%s','%s','%s','%s'" % (str(custs[i]),str(i+1),p,str(i_id),i_des,status)
+		sql = sql1+"pay_by"+sql2+s+sql3
+		file.write(sql)
+
+with open("payment.sql",'w') as file:
+	for i in range(num_orders):
+		delt = int(np.random.uniform()*365)
+		deltas = datetime.timedelta(days = delt)
+		sd = sdate+deltas
+		disc = int(np.random.uniform()*26)
+		s = "'%s','%s','%s'"%(str(i+1),str(sd),str(disc))
+		sql = sql1+"payment"+sql2+s+sql3
+		file.write(sql)
+
+
+delta = datetime.timedelta(days = 1)
+with open("tracks.sql",'w') as file:
+	stdt = sdate
+	while stdt<=end_date:
+		ids = np.random.choice(list(np.arange(num_staff)+1),4)
+		for i in range(4):
+			s = "'%s','%s'"%(str(ids[i]),str(stdt))
+			sql = sql1+"tracks"+sql2+s+sql3
+			file.write(sql)
+		stdt+=delta
+
+with open("delivers.sql",'w') as file:
+	for i in range(num_orders):
+		d_id = np.random.choice(list(np.arange(num_dishes)+1))
+		j = np.random.choice(list(np.arange(num_waiters)+num_owners+num_managers+num_receptionists))
+		completed = "1"
+		s = "'%s','%s','%s','%s'" % (str(j),str(i+1),str(d_id),completed)
+		sql = sql1+"delivers"+sql2+s+sql3
+		file.write(sql)
+
 
 
 
